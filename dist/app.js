d516764bb28d845746c211fffa6979779e4f8aa3
zvasileva <zojka-homka@rambler.ru>
2020-05-24 00:50:00 +0300

    Задача 1

---
+++ dist/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+require("core-js/modules/es.array.filter");
+
+var characters = [{
+  name: 'мечник',
+  health: 10
+}, {
+  name: 'маг',
+  health: 100
+}, {
+  name: 'маг',
+  health: 0
+}, {
+  name: 'лучник',
+  health: 0
+}];
+var alive = characters.filter(function (item) {
+  return item.health > 0;
+});
